42027ec8df4693101525da4e2d17c3a392422230
greetingsgoods <blololob078@gmail.com>
2021-06-30 11:51:47 +0300

    vue add vuex && router

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import router from './router'
+import store from './store'
+import Axios from 'axios'
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
