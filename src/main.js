cca6a41528c5f294db0bd59db908e66f23828368
greetingsgoods <blololob078@gmail.com>
2021-06-30 11:27:15 +0300

    теперь  можем использовать this.$http в компонентах

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Axios from 'axios'
+
+Vue.prototype.$http = Axios;
+
+const token = localStorage.getItem('user-token')
+if (token) {
+    Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
+
 Vue.config.productionTip = false
 
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
